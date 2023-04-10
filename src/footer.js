b7d69213daa828e4a443246a72ed6d428c60e6a8
Harshvardhan Jha <67819701+harsh1kashyap@users.noreply.github.com>
2023-04-10 21:38:18 +0530

    Book Finder App
    First commit

---
+++ src/footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <h4>
+          &#60;&#47;&#62; with ❤️ & ☕ by{" "}
+          <a
+            style={{
+              textDecoration: "underline",
+              color: "white",
+            }}
+            href="https://github.com/harsh1kashyap"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Harshvardhan Jha
+          </a>
+        </h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
